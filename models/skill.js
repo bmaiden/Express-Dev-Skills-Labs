bf2f010ee169876049aced40c990e9f4e4dc19e6
bmaiden <b.maiden2012@yahoo.com>
2023-12-10 13:27:29 -0600

    adds models/skill.js

---
+++ models/skill.js
@@ -0,0 +1,14 @@
+const skills = [
+    {id:125223, skill: "HTML", done: true},
+    {id:127904, skill: "CSS", done: true},
+    {id:139608, skill: "JavaScript", done: false},
+    {id:139808, skill: "Express", done: true}
+];
+
+module.exports = {
+    getAll
+  };
+
+  function getAll() {
+    return skills;
+  };
