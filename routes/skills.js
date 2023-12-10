0cf010a19ba7fc0b0642e41ab5b513d56744fd3e
bmaiden <b.maiden2012@yahoo.com>
2023-12-10 14:34:11 -0600

    adds controllers/skills.js

---
+++ routes/skills.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+// require the controller that exports Skill CRUD functions
+var skillsCtrl = require('../controllers/skills');
+
+/* GET users listing. */
+// router.get('/', function(req, res, next) {
+  // res.send('respond with a resource');
+// });
+// all actual paths start with "/skills" so commented out the above router.get
+
+// GET /skills
+router.get('/', skillsCtrl.index);
+
+module.exports = router;
